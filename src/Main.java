cd55e9524453883f92ca5df87fa2b4a10d36112a
Lera Lyakisheva <leralyakisheva@gmail.com>
2021-11-26 12:48:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 2000000000;
+        int transaction = 500000000;
+        int total = currentAccount + transaction;
+        System.out.println(total);
+    }
+}
